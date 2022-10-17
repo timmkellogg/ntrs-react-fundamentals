22754880916519dc449d7386bffb1332c9311e98
Tim Kellogg <timmkellogg@gmail.com>
2022-10-17 18:11:00 -0400

    end of day 1

--- day-1/demos/07-typescript/script.ts
+++ day-1/demos/07-typescript/script.ts
@@ -1,3 +1,11 @@
 function getFavoriteNumber(): number {
     return 26;
 }
+
+const car: { make: string, model: string, yearBuilt: number, selfDriving: boolean } = {
+    make: 'Honda',
+    model: 'Civic',
+    yearBuilt: 2017,
+    selfDriving: false
+};
+    
